36f5be4a92ec4af506f4ed2b2da3a0f3c993b459
root <root>
2019-07-28 21:17:25 +0800

    aaa

---
+++ src/com/itcast/first.java
@@ -0,0 +1,13 @@
+package com.itcast;
+
+public class first {
+    public static void main(String[] args) {
+        System.out.println("lalaalla");
+        System.out.println("lalaalla");
+        System.out.println("lalaalla");
+        System.out.println("lalaalla");
+        System.out.println("lalaalla");
+        System.out.println("lalaalla");
+    }
+
+}
